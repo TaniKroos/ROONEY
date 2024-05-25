540276f9623e76b220daeba74b969a09ee497478
TaniKroos <tanishsaini26@gmail.com>
2024-05-25 22:30:26 +0530

    GLORY GLORY MAN UNITED

--- src/App.jsx
+++ src/App.jsx
@@ -1,17 +1,48 @@
-import { useState } from 'react'
-
+import { useState , useEffect} from 'react'
+import { useDispatch } from 'react-redux'
+import authService from './appwrie/auth'
 import './App.css'
+import authSlice from './store/authSlice'
+import {login , logout} from './store/authSlice'
+import { Header , Footer } from './components'
 
+import { Outlet } from 'react-router-dom'
 function App() {
-  const [count, setCount] = useState(0)
+  const [loading , setLoading] = useState(true)
+  const dispatch = useDispatch()
+
+  useEffect(() => {
+    authService.getCurrentUser()
+    .then((userData)=> {
+      if(userData){
+        dispatch(login({userData}))
+      }else{
+        dispatch(logout())
+      }
+    })
+    .finally(() => setLoading(false))
+  },[])
+
+
+
+
   console.log(import.meta.env.VITE_APPWRITE_URL)
-  return (
-    <>
-      <div className='p-4 bg-red-300'>
-        <h1 className=''>Wayne Rooney</h1>
-      </div>
-    </>
-  )
+
+  return !loading ? (
+    <div className='min-h-screen flex flex-wrap
+    content-between bg-gray-400'>
+
+          <div className='w-full block'>
+              <Header />
+              <main>
+                {/* {<Outlet/>} */}
+              </main>
+
+              <Footer />
+          </div>
+
+    </div>
+  ) : null
 }
 
 export default App
--- src/appwrie/auth.js
+++ src/appwrie/auth.js
@@ -64,4 +64,4 @@ export class AuthService {
 
 const authService = new AuthService();
 
-export default AuthService;
+export default authService;
